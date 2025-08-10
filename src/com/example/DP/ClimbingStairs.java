a9e7f0807b7e17c7a30a38955d0dddaf962d857b
Abhi <abhi.yadavh2o@gmail.com>
2025-08-10 23:17:38 +0530

    Climbing Stairs

--- src/com/example/DP/ClimbingStairs.java
+++ src/com/example/DP/ClimbingStairs.java
@@ -2,6 +2,7 @@ package com.example.DP;
 import java.util.Arrays;
 
 public class ClimbingStairs {
+    //memoization
     public static int climbingStairs(int n, int[] dp){
         if(n == 0){
             return 1;
@@ -13,11 +14,27 @@ public class ClimbingStairs {
         return dp[n];
     }
 
+    //tabulation
+    public static int climbingStairs_t(int n, int[] dp){
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+
+        dp[n] = climbingStairs_t(n-1,dp) + climbingStairs_t(n-2,dp);
+        return dp[n];
+    }
+
     public static void main(String[] args){
         int n = 5;
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
         int result = climbingStairs(n,dp);
-        System.out.println("Number of ways to climb the stairs are:- " + result);
+        int answer = climbingStairs_t(n,dp);
+        System.out.println("Number of ways to climb the stairs are:- " + result + " by memoization");
+        System.out.println("Number of ways to climb the stairs are:- " + answer + " by tabulation");
+
     }
 }
