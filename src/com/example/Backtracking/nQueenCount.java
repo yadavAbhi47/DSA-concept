38e816966efe54e126e92c06f9bf4980e8960cef
Abhi <abhi.yadavh2o@gmail.com>
2025-09-24 20:16:21 +0530

    nQueen

--- src/com/example/Backtracking/nQueenCount.java
+++ src/com/example/Backtracking/nQueenCount.java
@@ -1,7 +1,9 @@
 package com.example.Backtracking;
 import java.util.*;
 
-public class nQueen {
+public class nQueenCount {
+
+
     public static boolean checkPosition(char[][] board,int row,int col){
         //as we start from top to down in the row, we need not check the current position
         //as we place single queen in each row hence we not need to check left and right.
@@ -14,7 +16,7 @@ public class nQueen {
             }
         }
 
-        // check at the diagonally left (upward)
+        // check diagonally left (upward)
         for(int i=row,j=col; i>=0 && j>=0; i--,j--){
             if(board[i][j] == 'Q'){
                 return false;
@@ -30,20 +32,21 @@ public class nQueen {
         return true;
     }
 
-    public static void printBoard(char[][] board){
-        System.out.println("----------Chess Board----------");
-        for(int i = 0;i<board.length;i++){
-            for(int j = 0; j<board.length;j++){
-                System.out.print(board[i][j]);
-            }
-            System.out.println();
-        }
-    }
+//    public static void printBoard(char[][] board){
+//        System.out.println("----------Chess Board----------");
+//        for(int i = 0;i<board.length;i++){
+//            for(int j = 0; j<board.length;j++){
+//                System.out.print(board[i][j]);
+//            }
+//            System.out.println();
+//        }
+//    }
 
     public static void nQueen(char[][] board,int row){
 
         if(row>=board.length){
-            printBoard(board);
+//            printBoard(board);
+            count++;
             return;
         }
         for(int j = 0; j<board.length;j++){
@@ -56,6 +59,8 @@ public class nQueen {
         }
     }
 
+    static int count = 0;
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of chess-board: ");
@@ -66,6 +71,7 @@ public class nQueen {
                 board[i][j] = 'x';
             }
         }
-        nQueen(board,0);
+         nQueen(board,0);
+        System.out.println(count);
     }
 }
