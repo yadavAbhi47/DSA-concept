a9020faa98ec3001261bd22bead41af619e1f6fb
Abhi <abhi.yadavh2o@gmail.com>
2025-10-18 17:20:01 +0530

    Grid Ways

---
+++ src/com/example/HashMap/LargestSubArrayWithSumZero.java
@@ -0,0 +1,23 @@
+package com.example.HashMap;
+
+import java.util.HashMap;
+
+public class LargestSubArrayWithSumZero {
+    public static void main(String[] args) {
+        int[] arr = {15,-2,2,-8,1,7,10,23};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        int len = 0;
+
+        for(int i=0; i<arr.length;i++){
+            sum += arr[i];
+            if(map.containsKey(sum)){
+                len = Math.max(len,i-map.get(sum));
+            } else {
+                map.put(sum,i);
+            }
+        }
+        System.out.println("Largest subarray with sum as 0 is " + len);
+    }
+}
+
