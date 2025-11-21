c4ecd58e7267cc1a4bec7fcb7acb4582018dd2d7
Abhi <abhi.yadavh2o@gmail.com>
2025-11-22 02:19:31 +0530

    Searching in tries

--- src/com/example/Tries/Main.java
+++ src/com/example/Tries/Main.java
@@ -19,8 +19,6 @@ public class Main {
 
     public static void insert(String word){
         Node curr = root;
-
-
         for(int level=0; level<word.length();level++){
             int idx = word.charAt(level)-'a';
             if(curr.children[idx] == null){
@@ -31,11 +29,24 @@ public class Main {
         curr.eow = true;
     }
 
+    public static boolean search(String word){
+        Node curr = root;
+        for(int level=0; level<word.length();level++){
+            int idx = word.charAt(level)-'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
     public static void main(String[] args) {
         String[] words = {"the", "a", "there","their","any","three"};
 
         for(String word : words){
             insert(word);
         }
+        System.out.println(search("their"));
     }
 }
