73020c1a58a7be3d162c7839abaeb6bc81ed05f4
Abhi <abhi.yadavh2o@gmail.com>
2025-12-21 01:55:23 +0530

    Rod cutting problem solved

---
+++ src/com/example/DP/RodCutting.java
@@ -0,0 +1,28 @@
+package com.example.DP;
+
+public class RodCutting {
+
+    public static int rodCutting(int[] length, int[] price, int totalRod){
+        int n = price.length;
+        int[][] dp = new int[n+1][totalRod+1];
+
+        for(int i = 1; i<n+1;i++){
+            for(int j = 1; j<totalRod+1;j++){
+                if(length[i-1]<=j){
+                    dp[i][j] = Math.max(price[i-1]+ dp[i][j-length[i-1]], dp[i-1][j]);
+                } else{
+                    dp[i][j] = dp[i-1][j];
+                }
+
+            }
+        }
+        return dp[n][totalRod];
+    }
+
+    public static void main(String[] args) {
+        int[] price = {1,5,8,9,10,17,17,20};
+        int[] length = {1,2,3,4,5,6,7,8};
+        int totalRod = 8;
+        System.out.println(rodCutting(length,price,totalRod));
+    }
+}
