485a28096bfdd20f0d9c9d36e76b71c70a428f92
Abhi <abhi.yadavh2o@gmail.com>
2025-12-23 01:36:06 +0530

    LCS memoization

---
+++ src/com/example/DP/LCS/LCSmemo.java
@@ -0,0 +1,35 @@
+package com.example.DP.LCS;
+
+import java.util.Arrays;
+
+public class LCSmemo {
+
+    public static int lcsmemo(String str1, String str2,int n, int m, int[][] dp){
+        if(n==0 || m==0){
+            return 0;
+        }
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
+
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+            return dp[n][m] = lcsmemo(str1, str2, n-1,m-1,dp) + 1;
+        } else{
+            int ans1 = lcsmemo(str1,str2,n-1,m,dp);
+            int ans2 = lcsmemo(str1,str2,n,m-1,dp);
+            return dp[n][m] = Math.max(ans1,ans2);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcdge";
+        String str2 = "abedg";
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        System.out.println(lcsmemo(str1, str2, n, m, dp));
+    }
+}
